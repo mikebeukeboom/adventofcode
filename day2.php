e955218b7eae770c341e2669425fe5e6a8f34ca5
= <=>
2017-12-05 18:00:31 +0100

    Added the solution of day2 part1

---
+++ day2.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 05/12/2017
+ * Time: 15:09
+ */
---
+++ day2/day2.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 05/12/2017
+ * Time: 17:18
+ */
+
+$content = file('input.txt');
+$i = 0;
+$answer = 0;
+$count = array();
+foreach ($content as $row) {
+    $row = str_replace("	", " ", $row);
+    $row = str_replace("\n","", $row);
+    $count[$i] = explode(" ", $row);
+    $i++;
+}
+
+foreach ($count as $number){
+    $min = min($number);
+    $max = max($number);
+    $difference = (int)$max - (int)$min;
+    $answer += $difference;
+}
+echo $answer;
