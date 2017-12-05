02dd74e11a9d4c74e250f8fbe9c636d326614b71
= <=>
2017-12-05 18:32:34 +0100

    cleanup

--- day2/day2.php
+++ day2/day2.php
@@ -5,21 +5,14 @@
  * Date: 05/12/2017
  * Time: 17:18
  */
-
 $content = file('input.txt');
-$i = 0;
 $answer = 0;
-$count = array();
 foreach ($content as $row) {
     $row = str_replace("\n","", $row);
-    $count[$i] = explode("	", $row);
-    $i++;
-}
-
-foreach ($count as $number){
-    $min = min($number);
-    $max = max($number);
+    $numberList = explode("\t", $row);
+    $min = min($numberList);
+    $max = max($numberList);
     $difference = (int)$max - (int)$min;
     $answer += $difference;
 }
-echo $answer;
+echo $answer; //43074
